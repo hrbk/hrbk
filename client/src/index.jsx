50c506d22f9606b5414695377d747fe4036c7b06
cmehrabian <cameron.mehrabian@gmail.com>
2017-08-09 15:57:43 -0700

    adds basic jest tests + deps

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
