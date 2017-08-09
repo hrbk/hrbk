f04c8f5e481d60802ad8b2c63d332a45d89d4ed7
Austin Reuter <abreuter101@gmail.com>
2017-08-08 19:40:40 -0700

    Add proc file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 8080;
 
-
-// app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/', function(req, res) {
+
   res.send('Hello World');
 });
 
 
-app.listen(3000, function() {
-	console.log('Listening on port: ', 3000);
+app.listen(port, () => {
+	console.log('process.env.PORT:', process.env.PORT); 
 });
