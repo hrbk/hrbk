8f7df9546a7390351d2611f7f7a8f64e7c768323
Austin Reuter <abreuter101@gmail.com>
2017-08-08 18:53:49 -0700

    Update dependencies

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '/../client/dist'));
+// app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
 
 
-app.listen(3000);
+app.listen(3000, function() {
+	console.log('Listening on port: ', 3000);
+});
