f40ce4304e0759d7334d99625aa970a6fbd8f619
Daniel Cervoni <dcervoni03@gmail.com>
2017-08-09 14:55:41 -0700

    rendered searchbar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        APP
+        <Search></Search>
       </div>
     );
   }
