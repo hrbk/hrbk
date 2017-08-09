723366ba7365c7ec08197ad1b6629961f0cb2490
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-09 13:29:54 -0700

    add require and create shallow connection

---
+++ database/index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const createTables = require(./config);
+const Promise = require('bluebird');
+const database = 'homeswap';
+
+const connection = mysql.createConnection({
+  user: 'root',
+
+});
+
+
+
+
+module.exports 
