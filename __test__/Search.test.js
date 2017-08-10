1dd7686407387ad5eadc84520b50d64c64ffe965
Daniel Cervoni <dcervoni03@gmail.com>
2017-08-10 12:16:59 -0700

    implemented handling of search input

--- __test__/Search.test.js
+++ __test__/Search.test.js
@@ -3,6 +3,13 @@ import { mount } from 'enzyme';
 // import App from '../client/src/index.jsx';
 import Search from '../client/src/components/Search.jsx';
 
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
 // test('Check searchTerm state is being tracked', () => {
 // 	const wrapper = mount(
 //     <Search/>
