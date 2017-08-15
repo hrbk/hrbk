6962c8abe737dd10dc447738d3ccd7b1c2a5ba3b
Austin Reuter <abreuter101@gmail.com>
2017-08-14 17:18:01 -0700

    Create filterByCity, addListing, filterByOption db FNs
    -refractor with standardized tests and foreign key implementation

--- database/helpers.js
+++ database/helpers.js
@@ -27,7 +27,57 @@ var addUser = function(email, userphoto, firstname, lastname, password) {
   db.query(sql, options);
 };
 
+//TODO: create USER FOREIGN KEY functionality
+var addListing = function(userObj) {
+  //if user is going to be sent as stringified object;
+  user = JSON.parse(userObj);
+
+  var options = [
+    user.id, user.address, user.city, user.state, 
+    user.zipcode, user.title, user.description, user.photopath
+  ]; 
+
+  var sql = `
+    INSERT INTO profiles (id, address, city, state, zipcode, title, description, photopath)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
+  db.query(sql, options);
+};
+
+//TEST: add a listing;
+var listing = {
+  id: '223',
+  address: '222',
+  city: 'new york',
+  state: 'california',
+  zipcode: '94109',
+  title: 'title', 
+  description: 'description house',
+  photopath: null
+};
+  //addListing(JSON.stringify(listing))
+  //uncomment, and change the above to add a listing
+//end of add listing test
+
+
+var filterByCity = function(city, callback) {
+  var sql = `
+    SELECT * FROM profiles WHERE city = "${city}";`;
+  db.query(sql, callback)
+};
+
+var filterByOption = function(column, option, callback) {
+  var sql = `
+    SELECT * FROM profiles WHERE ${column} = "${option}";`;
+  db.query(sql, callback);
+};
+
+//TEST: filter by city.
+filterByOption('city', 'san francisco', function(err, res) {
+  console.log('TEST: filterByOption:(city, san francisco)', res);
+});
+//end of filter by city test
 
 module.exports.find = find;
 module.exports.addUser = addUser;
+module.exports.filterByCity = filterByCity;
 
