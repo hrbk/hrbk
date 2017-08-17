3ebf9ed6f645f53aa79cee80d05b69048abfbfe4
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-16 19:01:59 -0700

    create new profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,1 +1,39 @@
 import React from 'react'; 
+import data from '../../../testdata.json'; 
+import * as rb from 'react-bootstrap';
+
+var title = {
+  position: 'relative',
+  background: `url(${data.profiles[1].photopath}) center center`, 
+  height: '70vh',
+  backgroundSize: 'cover',
+  overflow: 'hidden'
+}
+var profilePhoto = {
+  background: `url(${data.profiles[1].photopath}) no-repeat center`, 
+  backgroundSize: '100% auto',
+  height: '80vh'
+}
+
+class Profile extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      
+    }
+  }
+  
+  render() {
+    return (
+    <div>
+
+      <rb.Jumbotron style={title}>
+        
+      </rb.Jumbotron>
+    </div>
+    )
+  }
+  
+}
+
+export default Profile;
