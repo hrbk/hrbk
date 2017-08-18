eeeb148ba67e71a3d9a9b295b590dc6d0bf86e1b
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-17 21:49:42 -0700

    add documentation for index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const path = require('path');
 const dbHelpers = require('../database/helpers')
 const bodyParser = require('body-parser');
 
-
 const app = express();
+/**
+ * Port is set either to the process.env.PORT given by Heroku or defaults to 3000
+ */
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,21 +15,21 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+/**
+ * Get method that receives a request from the client side and passes it towards the database. The request will be formatted by being served through Google Autocomplete Places. Conditionals are set in place in order of locality depending on what the users may query in the search bar. City is the most ideal, but the filter function for the database will default if no city is defined. The same goes for state. Currently, although a default for country exists within this function, the database does not have a column for country. There currently is no error handling; just a res.json for the data received.
+ */
 app.get('/search', function(req, res) {
 	const { city, state, country } = JSON.parse(req.query.data);
-
 	if ( city ) {
 		dbHelpers.filterByCity(city, (data) => {
 			res.json(data);
 		});
 	}
-
 	else if ( state ) {
 		dbHelpers.filterByOption('state', state, (data) => {
 			res.json(data);
 		});
 	}
-
 	else if ( country ) {
 		dbHelpers.filterByOption('country', country, (data) => {
 			res.json(data);
