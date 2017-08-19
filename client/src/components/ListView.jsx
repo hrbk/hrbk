6090e49a4bae97794e5440c48c17ed229150831a
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-18 21:05:01 -0700

    add documentation for ListView

--- client/src/components/ListView.jsx
+++ client/src/components/ListView.jsx
@@ -4,9 +4,11 @@ import Header from './Header.jsx';
 import View from './View.jsx';
 import Search from './Search.jsx';
 import data from '../../../testdata.json';
-//have a link to all the views url.. or something along those lines.
 
 const ListView = (props) => (
+  /**
+   * The ListView component maps through all filtered homes from a user's search into a list of div contained pieces of information. 
+   */
     <div>
       {props.profiles.map((home, i) => <View key={i} home={home}/>)}
     </div>
