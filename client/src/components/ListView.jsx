6372917272e68248f78e31ceba4c29276a719855
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-19 10:08:12 -0700

    comment added back to ListView

--- client/src/components/ListView.jsx
+++ client/src/components/ListView.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import View from './View.jsx';
+<<<<<<< HEAD
 //import data from '../../../testdata.json';
 //have a link to all the views url.. or something along those lines.
+=======
+import Search from './Search.jsx';
+import data from '../../../testdata.json';
+>>>>>>> add documentation for ListView
 
 const ListView = (props) => (
+  /**
+   * The ListView component maps through all filtered homes from a user's search into a list of div contained pieces of information. 
+   */
     <div>
       {props.profiles.map((home, i) => <View key={i} home={home}/>)}
     </div>
