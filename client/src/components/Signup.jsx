d7d1384139bb76e79b7799d186fd8d36c0db68b8
cmehrabian <cameron.mehrabian@gmail.com>
2017-08-20 11:14:29 -0700

    Integrates react dropzone imageUpload component

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as rb from 'react-bootstrap';
 import axios from 'axios';
+import ImageUpload from 'react-dropzone';
 
 var spacing = {
   marginBottom: '10px'
@@ -34,50 +35,54 @@ class Signup extends React.Component {
                 <rb.ControlLabel>Email</rb.ControlLabel>
                 <rb.FormControl
                   style={spacing}
-                  type="text" 
-                  name="email" 
-                  value={this.props.userInfo.email} 
+                  type="text"
+                  name="email"
+                  value={this.props.userInfo.email}
                   placeholder="Email"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'email')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Profile Picture</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing}             
-                  type="text" 
-                  name="userphoto"  
-                  value={this.state.userphoto} 
-                  placeholder="Profile Picture" 
+                <ImageUpload
+                  accept="image/jpeg, image/png">
+                  <div>Upload Image</div>
+                </ImageUpload>
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
+                  name="userphoto"
+                  value={this.state.userphoto}
+                  placeholder="Profile Picture"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'userphoto')}>
                 </rb.FormControl>
-                
+
                 <rb.ControlLabel>First Name</rb.ControlLabel>
-                <rb.FormControl 
+                <rb.FormControl
                   style={spacing}
-                  type="text" 
-                  name="firstname" 
-                  value={this.state.firstname} 
-                  placeholder="First Name" 
+                  type="text"
+                  name="firstname"
+                  value={this.state.firstname}
+                  placeholder="First Name"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'firstname')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Last Name</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="lastname"
-                  value={this.state.lastname} 
-                  placeholder="Last Name" 
+                  value={this.state.lastname}
+                  placeholder="Last Name"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'lastname')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Password</rb.ControlLabel>
                 <rb.FormControl
-                  style={spacing} 
+                  style={spacing}
                   type="text"
                   name="password"
                   value={this.state.password}
@@ -87,77 +92,77 @@ class Signup extends React.Component {
                 </rb.FormControl>
 
                 <rb.ControlLabel>Address</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="address"
-                  value={this.state.address} 
-                  placeholder="Address" 
+                  value={this.state.address}
+                  placeholder="Address"
                   maxLength={100}
                   onChange={this.handleChange.bind(this, 'address')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>City</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="city"
-                  value={this.state.city} 
+                  value={this.state.city}
                   placeholder="City"
                   maxLength={25}
                   onChange={this.handleChange.bind(this, 'city')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>State</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="state"
-                  value={this.state.state} 
-                  placeholder="State" 
+                  value={this.state.state}
+                  placeholder="State"
                   maxLength={10}
                   onChange={this.handleChange.bind(this, 'state')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Zip Code</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="zipcode"
-                  value={this.state.zipcode} 
-                  placeholder="Zip Code" 
+                  value={this.state.zipcode}
+                  placeholder="Zip Code"
                   onChange={this.handleChange.bind(this, 'zipcode')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Home Title</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="title"
-                  value={this.state.title} 
+                  value={this.state.title}
                   placeholder="Home Title"
-                  maxLength={25} 
+                  maxLength={25}
                   onChange={this.handleChange.bind(this, 'title')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Home Description</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="description"
-                  value={this.state.description} 
-                  placeholder="Home Description" 
+                  value={this.state.description}
+                  placeholder="Home Description"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'description')}>
                 </rb.FormControl>
 
                 <rb.ControlLabel>Home Photo</rb.ControlLabel>
-                <rb.FormControl 
-                  style={spacing} 
-                  type="text" 
+                <rb.FormControl
+                  style={spacing}
+                  type="text"
                   name="photopath"
-                  value={this.state.photopath} 
-                  placeholder="Add a Home Photo" 
+                  value={this.state.photopath}
+                  placeholder="Add a Home Photo"
                   maxLength={50}
                   onChange={this.handleChange.bind(this, 'photopath')}>
                 </rb.FormControl>
